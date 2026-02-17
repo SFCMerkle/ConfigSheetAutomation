bf99bec2aa40c13cc09a42d6eed100ade1094e18
Merkle <cmerkle@idexcorp.com>
2026-02-17 10:57:49 +0100

    initial commit

---
+++ Read_ConfSheet.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+
+
+# Path to your Excel file
+file_path = "C:/Users/cmerkle/OneDrive - IDEX Corporation/Desktop/CS-BRAC055U007.xlsx"
+
+
+
+# Read the Excel file
+df = pd.read_excel(file_path)
+
+# Display the first few rows
+print(df.head())
+
+
+
+
+
+
