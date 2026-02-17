f9d59569ea7282cdc9b9cc9eada52587845951f4
Merkle <cmerkle@idexcorp.com>
2026-02-17 12:06:01 +0100

    first test, read configsheet version

--- Read_ConfSheet.py
+++ Read_ConfSheet.py
@@ -1,20 +1,18 @@
 
 import pandas as pd
+import openpyxl as pxl
 
 
 # Path to your Excel file
-file_path = "C:/Users/cmerkle/OneDrive - IDEX Corporation/Desktop/CS-BRAC055U007.xlsx"
-
-
+file_path = "C:/Users/cmerkle/OneDrive - IDEX Corporation/Desktop/Configsheet_Project/CS-BRAC055U007.xlsx"
+#print(f"Filename: {file_path}")
 
 # Read the Excel file
-df = pd.read_excel(file_path)
+Config_file = pxl.load_workbook(file_path, data_only=True)
+last_sheet = Config_file[Config_file.sheetnames[-1]]
 
-# Display the first few rows
-print(df.head())
+# Display the second row of the last sheet
 
-
-
-
+print(f"S2, Version: {last_sheet['S2'].value}")
 
 
